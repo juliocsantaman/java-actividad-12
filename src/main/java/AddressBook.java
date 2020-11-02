540dfb23e2d87724fe2344585ce9fd03a6b7a03d
juliocsantaman <juliocsantaman@gmail.com>
2020-11-02 00:12:32 -0500

    Realizando los requerimientos de la actividad 12.

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -1,13 +1,141 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Julio Cesar Santaman Cruz.
  */
 public class AddressBook {
+
+    // Atributos.
+    HashMap<String, String> contacts = new HashMap<String, String>();
+    Scanner scanner = new Scanner(System.in);
+
+    // Métodos.
     
+    /*
+    Este método cargará la información del archivo al HashMap.
+    */
+    public void load(String file) {
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(file));
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] parts = line.split(",");
+                contacts.put(parts[0], parts[1]);
+                // System.out.println(line);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+    /*
+    Esté método guardará un contacto.
+    */
+    public void save(String file, String phone, String name) {
+        BufferedWriter bufferedWriter = null;
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(file, true);
+            bufferedWriter = new BufferedWriter(fw);
+            bufferedWriter.write("\n" + phone + "," + name);
+            System.out.println("Contacto guardado");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (bufferedWriter != null) {
+                    bufferedWriter.close();
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+    
+    /*
+    Esté método mostrará los contactos de la agenda.
+    */
+    public void list() {
+        System.out.println("Contactos:");
+        contacts.forEach((k, v) -> System.out.println("Número: " + k + " : Nombre: " + v));
+    }
+    
+    /*
+    Este método creará un contacto.
+    */
+    public void create(String file) {
+        String name, phone;
+        // Solicitando los datos.
+        System.out.println("Ingresa nombre del contacto:");
+        name = scanner.next();
+        System.out.println("Ingresa el número teléfonico del contacto:");
+        phone = scanner.next();
+        // Instanciamos un objeto AddressBook.
+        AddressBook addressBook = new AddressBook();
+        // Llamamos al método save para guardar el nuevo contacto creado.
+        addressBook.save(file, phone, name);
+    }
+    
+    /*
+    Esté método eliminará un contacto dependiendo del número teléfonico.
+    */
+    public void delete(String file) {
+        String phone;
+        int c = 0;
+        // Solicitando datos.
+        System.out.println("Ingrese el número teléfonico para eliminarlo:");
+        phone = scanner.next();
+        // Eliminando el contacto del HashMap.
+        contacts.remove(phone);
+        System.out.println("Contacto borrado");
+
+        BufferedWriter bufferedWriter = null;
+
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(file));
+            // Actualizando la información del archivo.
+            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                if(c == 0) {
+                    bufferedWriter.write(entry.getKey() + "," + entry.getValue());
+                    c++;
+                } else {
+                    bufferedWriter.write("\n" + entry.getKey() + "," + entry.getValue());
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (bufferedWriter != null) {
+                    bufferedWriter.close();
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,79 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Julio Cesar Santaman Cruz.
  */
 public class Main {
+
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        AddressBook addressBook = new AddressBook();
+        String file = "contacts.txt";
+        String option;
+        String res = "";
+
+        do {
+            System.out.println("***** Bienvenido a tu Agenda Telefónica *****");
+            System.out.println("1. Mostrar los contactos de la agenda.");
+            System.out.println("2. Crear un nuevo contacto.");
+            System.out.println("3. Borrar un contacto.");
+            System.out.println("0. Salir.");
+            System.out.println("Elige una opción: ");
+            option = scanner.next();
+            
+            switch(option) {
+                case "1":
+                    // Mostrar los contactos de la agenda.
+                    addressBook.load(file);
+                    addressBook.list();
+                    break;
+                    
+                case "2":
+                    // Crear un nuevo contacto.
+                    addressBook.create(file);
+                    break;
+                    
+                case "3":
+                    // Borrar un contacto.
+                    addressBook.delete(file);
+                    break;
+                    
+                case "0":
+                    // Salir de la app.
+                    System.out.println("Aplicación finalizada.");
+                    System.exit(0);
+
+                default:
+                    System.out.println("Opción no válida.");
+            }
+            
+            do {
+                System.out.println("¿Deseas volver a ejecutar el programa?");
+                System.out.println("Escribe si o no: ");
+                scanner.nextLine();
+                res = scanner.next();
+                res = res.toLowerCase();
+
+                if (!res.equals("si") && !res.equals("no")) {
+                    System.out.println("No has escrito una respuesta correcta.");
+                    System.out.println("Recuerda que solo es si o no.");
+                }
+
+            } while (!res.equals("si") && !res.equals("no"));
+            
+            System.out.println();
+            scanner.nextLine();
+
+        } while (res.equals("si"));
+
+        System.out.println("Aplicación finalizada.");
     }
-    
+
 }
